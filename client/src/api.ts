a372cf5143e2068bd615d7d820143bc0a8955900
imran.s.s <imranss1309@gmail.com>
2026-06-19 21:19:37 +0530

    Configure API URL for split Render/Vercel deployment

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:3000/api'
+  baseURL: import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}/api` 
+    : (import.meta.env.PROD ? '/api' : 'http://localhost:3000/api')
 });
 
 api.interceptors.request.use((config) => {
